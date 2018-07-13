aff3f259fe1495b4c6f8afc165e3160287c299b2
Paula Suarez <pauladzuck@hotmail.com>
2018-07-13 13:05:19 -0500

    Adding files to project folder.

---
+++ app.js
@@ -0,0 +1,14 @@
+let me = {
+    name: 'Paula Suarez',
+    career: 'VP of IT',
+    description: 'Passionate self-proclaimed wanna be developer!',
+    interests: ['travel', 'languages', 'cooking', 'sewing', 'writing']
+    
+};
+
+console.log('Name:' + ' '+ me.name.toUpperCase());
+console.log('Career:'+' ' + me.career);
+console.log('Description:'+' ' + me.description);
+console.log('My Interests:'+' ' + me.interests);
+
+
