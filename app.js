e754bf5ee98f58caee67cb4031b5dd48d8dafb12
Paula Suarez <pauladzuck@hotmail.com>
2018-07-19 13:00:43 -0500

    adding updates to .js file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-let me = {
+var me =  {
     name: 'Paula Suarez',
     career: 'VP of IT',
-    description: 'Passionate self-proclaimed wanna be developer!',
-    interests: ['travel', 'languages', 'cooking', 'sewing', 'writing']
-    
+    description: 'Crazy-passionate individual about making it!',
+    interests: ['travel', 'languages', 'cooking', 'sewing', 'writing'],
 };
 
-console.log('Name:' + ' '+ me.name.toUpperCase());
-console.log('Career:'+' ' + me.career);
-console.log('Description:'+' ' + me.description);
-console.log('My Interests:'+' ' + me.interests);
+console.log('Name:' + ' ' + me.name.toUpperCase());
+console.log('Career:' + ' ' + me.career);
+console.log('Description:' + ' ' + me.description);
+
+console.log('\n'+ 'My Interests:'+ '\n'  + '*' + me.interests[0] + '\n' + '*' + me.interests[1] + '\n' + '*' + me.interests[2] +'\n' + '*' + me.interests[3] + '\n' + '*' +  me.interests[4] + '\n');
 
 
