2cc861be3fa04cd53056adda52a934f0ccbaa933
Paula Suarez <pauladzuck@hotmail.com>
2018-07-25 02:06:16 -0500

    Added display function to the file.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var me =  {
-    name: 'Paula Suarez',
-    career: 'VP of IT',
-    description: 'Crazy-passionate individual about making it!',
-    interests: ['travel', 'languages', 'cooking', 'sewing', 'writing'],
-};
+//interests: ['travel', 'languages', 'cooking', 'sewing', 'writing'],
+//experience: ['Blah', 'blah', 'blah', 'blah'],
 
-console.log('Name:' + ' ' + me.name.toUpperCase());
-console.log('Career:' + ' ' + me.career);
-console.log('Description:' + ' ' + me.description);
 
-console.log('\n'+ 'My Interests:'+ '\n'  + '*' + me.interests[0] + '\n' + '*' + me.interests[1] + '\n' + '*' + me.interests[2] +'\n' + '*' + me.interests[3] + '\n' + '*' +  me.interests[4] + '\n');
+//console.log('Name:' + ' ' + me.name.toUpperCase());
+//console.log('Career:' + ' ' + me.career);
+//console.log('Description:' + ' ' + me.description);
+//console.log('\n'+ 'My Interests:'+ '\n'  + '*' + me.interests[0] + '\n' + '*' + me.interests[1] + '\n' + '*' + me.interests[2] +'\n' + '*' + me.interests[3] + '\n' + '*' +  me.interests[4] + '\n');
+//console.log('\n'+ 'My Experience:'+ '\n'  + '*' + me.experience[0] + '\n' + '*' + me.experience[1] + '\n' + '*' + me.experience[2] +'\n' + '*' + me.experience[3] + '\n' + '*' +  me.experience[4] + '\n');
+
+//created function called displayPosition that takes parameters for company name, job title, and description
+function displayPosition(name, career, description){
+    console.log (' Name:' + ' ' + name.toUpperCase()+ ''+ '\n'+ ' Career:'+ ' ' + career + '\n' + ' Description:'+ ' ' + description);
+}
+
+displayPosition('Paula Suarez', 'VP of IT', 'Crazy-passionate individual about making it!', );
 
 
